4ae29ca7aaadeb553dfe32d7fe4fc8cacf4a29d8
elkel53930 <elkel53930@gmail.com>
2017-11-14 11:31:26 +0900

    Cssモジュールを使った。

--- Signup/Signup.elm
+++ Signup/Signup.elm
@@ -2,6 +2,7 @@ module Signup exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Css
 
 type alias User =
   { name : String
@@ -59,42 +60,47 @@ view user =
       ]
     ]
 
+styles cssPairs =
+  Css.asPairs cssPairs
+    |> Html.Attributes.style
+
 headerStyle : Attribute msg
 headerStyle =
-  style [ ( "padding-left", "3cm" ) ]
+  styles
+    [ Css.paddingLeft (Css.cm 3) ]
 
 formStyle : Attribute msg
 formStyle =
-    style
-        [ ( "border-radius", "10px" )
-        , ( "background-color", "#f2f2f2" )
-        , ( "padding", "20px" )
-        , ( "width", "300px" )
+    styles
+        [ Css.borderRadius (Css.px 5)
+        , Css.backgroundColor (Css.hex "#f2f2f2")
+        , Css.padding (Css.px 20)
+        , Css.width (Css.px 300)
         ]
 
 inputTextStyle : Attribute msg
 inputTextStyle =
-  style
-    [ ("display", "block")
-    , ("width", "260px")
-    , ("padding", "12px 20px")
-    , ("margin", "8px 0")
-    , ( "border", "none" )
-    , ( "border-radius", "4px" )
+  styles
+    [ Css.display Css.block
+    , Css.width (Css.px 260)
+    , Css.padding2 (Css.px 12) (Css.px 20)
+    , Css.margin2 (Css.px 8) (Css.px 0)
+    , Css.border (Css.px 0)
+    , Css.borderRadius  (Css.px 4)
     ]
 
 
 buttonStyle : Attribute msg
 buttonStyle =
-  style
-    [ ( "width", "300px" )
-    , ( "background-color", "#397cd5" )
-    , ( "color", "white" )
-    , ( "padding", "14px 20px" )
-    , ( "margin-top", "10px" )
-    , ( "border", "none" )
-    , ( "border-radius", "4px" )
-    , ( "font-size", "16px" )
+  styles
+    [ Css.width (Css.px 300)
+    , Css.backgroundColor (Css.hex "#397cd5")
+    , Css.color (Css.hex "#fff")
+    , Css.padding2 (Css.px 14) (Css.px 20)
+    , Css.marginTop (Css.px 10)
+    , Css.border (Css.px 0)
+    , Css.borderRadius (Css.px 4)
+    , Css.fontSize (Css.px 16)
     ]
 
 update : msg -> User -> User
